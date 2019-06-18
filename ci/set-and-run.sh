c46dff700bdcf5460163ef1098e25df40fa0503f
Juan Pablo Genovese <juanpgenovese@gmail.com>
2019-06-18 12:53:19 +0200

    Rename pipline

--- ci/set-and-run.sh
+++ ci/set-and-run.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/env bash
 
-fly --target tutorial set-pipeline --pipeline test-deploy --config test-deploy-pipeline.yml \
+fly --target tutorial set-pipeline --pipeline container-demo --config pipeline.yml \
     --var "kubernetes-user-token=$(cat $HOME/work/user.token)" \
     --var "kubernetes-user-cert=$(cat $HOME/work/ca.crt)" \
     --var "kubernetes-api-server=$(cat $HOME/work/kubeurl)" \
     --var "dockerhub-id=$DOCKER_REPO" \
 		--var "dockerhub-username=$DOCKER_USERNAME" \
 		--var "dockerhub-password=$DOCKER_PASSWORD"
+
+fly --target tutorial unpause-pipeline -p container-demo
+fly --target tutorial trigger-job -w -j container-demo/create-and-publish-image
